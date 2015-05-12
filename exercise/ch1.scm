b533f1a5cfbae61fe97898b85be85dae0c3a6b84
Ato Araki <ato.araki@gmail.com>
2015-05-12 23:15:54 +0900

    ch1: add exercise

--- exercise/ch1.scm
+++ exercise/ch1.scm
@@ -5,6 +5,7 @@
 (test* "ex 1.2" (- (/ 37 150)) (/ (+ 5 4 (- 2 (- 3 (+ 6 (/ 4 5))))) (* 3 (- 6 2) (- 2 7))))
 
 ;;EXERCISE 1.3
+(define (square x) (* x x))
 (define (sum-of-squares x y)
   (+ (square x) (square y)))
 (define (f a b c)
@@ -26,5 +27,39 @@
 (test* "ex 1.4" 3 (a-plus-abs-b 1 -2))
 (test* "ex 1.4" 1 (a-plus-abs-b -1 2))
 
+;;EXECISE 1.5
+(define (p) (p))
+
+(define (test x y)
+  (if (= x 0)
+      0
+      y))
+
+;; (test 0 (p)) ; this line hang up, so gosh is applicative-order evaluation (適用順序評価)
+
+;;EXECISE 1.6
+(define (new-if predicate then-clause else-clause)
+  (cond (predicate then-clause)
+        (else else-clause)))
+
+(define (sqrt-iter guess x)
+  (new-if (good-enough? guess x)
+          guess
+          (sqrt-iter (improve guess x)
+                     x)))
+
+(define (improve guess x)
+  (average guess (/ x guess)))
+
+(define (average x y)
+  (/ (+ x y) 2))
+
+(define (good-enough? guess x)
+  (< (abs (- (square guess) x)) 0.001))
+
+(define (sqrt x)
+  (sqrt-iter 1.0 x))
+
+;: (sqrt 9) ; this line hang up.
 
 (test-end :exit-on-failure #t)
