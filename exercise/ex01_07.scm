aa9761b87e9615fdea3f357b9468ac31712b08e1
Ato Araki <ato.araki@gmail.com>
2015-05-24 15:09:26 +0900

    ex1.7: add test

--- exercise/ex01_07.scm
+++ exercise/ex01_07.scm
@@ -22,11 +22,12 @@
   (/ (+ x y) 2))
 
 (define (good-enough? guess old)
-  (< (abs (- 1.0 (/ guess old))) 0.001))
+  (< (abs (- 1.0 (/ guess old))) 1e-5))
 
 (define (sqrt x)
   (sqrt-iter 1.0 x))
 
 (test* "ex 1.7" 0.006324555320336759 (sqrt 0.00004) (expect-delta 1e-5))
+(test* "ex 1.7" 31.622776601683793 (sqrt 1000.0) (expect-delta 1e-5))
 
 (test-end :exit-on-failure #t)
