3ce272d7b375ca89f5522cd02751768457b0bfe6
Ato Araki <ato.araki@gmail.com>
2015-06-16 22:38:33 +0900

    ex1.11 update

--- scheme/ex01_11.scm
+++ scheme/ex01_11.scm
@@ -34,4 +34,19 @@
 (test* "ex 1.11" 25 (f 5))
 (test* "ex 1.11" 59 (f 6))
 
+(define (f n) 
+  (define (fn n1 n2 n3) (+ n1 (* 2 n2) (* 3 n3)))
+  (define (f-iter count n1 n2 n3) 
+    (if (= count 0)
+        n3
+        (f-iter (- count 1) (fn n1 n2 n3) n1 n2)))
+  (f-iter n 2 1 0))
+
+(test* "ex 1.11" 1 (f 1))
+(test* "ex 1.11" 2 (f 2))
+(test* "ex 1.11" 4 (f 3))
+(test* "ex 1.11" 11 (f 4))
+(test* "ex 1.11" 25 (f 5))
+(test* "ex 1.11" 59 (f 6))
+
 (test-end :exit-on-failure #t)
