2d1382781498bf35b33457949f2413f2eb23ad7f
Ato Araki <ato.araki@gmail.com>
2015-09-23 22:19:34 +0900

    ex1.24: use fast-prime

---
+++ scheme/ex01_24.scm
@@ -0,0 +1,101 @@
+(use gauche.test)
+(add-load-path "." :relative)
+(test-start "ch1")
+
+;;EXERCISE 1.24
+
+;; for gauche
+(use srfi-27)
+(define true #t)
+(define false #f)
+(define (random i) (random-integer i))
+
+;; fast-prime?
+(define (expmod base exp m)
+  (cond ((= exp 0) 1)
+        ((even? exp)
+         (remainder (square (expmod base (/ exp 2) m))
+                    m))
+        (else
+         (remainder (* base (expmod base (- exp 1) m))
+                    m))))        
+
+(define (fermat-test n)
+  (define (try-it a)
+    (= (expmod a n n) a))
+  (try-it (+ 1 (random (- n 1)))))
+
+(define (fast-prime? n times)
+  (cond ((= times 0) true)
+        ((fermat-test n) (fast-prime? n (- times 1)))
+        (else false)))
+
+;; test
+(define (timed-prime-test n)
+  (newline)
+  (display n)
+  (start-prime-test n (runtime)))
+
+(define (start-prime-test n start-time)
+  (if (fast-prime? n 1000)
+      (report-prime (- (runtime) start-time))))
+
+(define (report-prime elapsed-time)
+  (display " *** ")
+  (display elapsed-time))
+
+;; for gauche
+(define (runtime)
+  (use srfi-11)
+  (let-values (((a b) (sys-gettimeofday)))
+    (+ (* a 1000000) b)))
+
+(define (smallest-divisor n)
+  (find-divisor n 2))
+
+(define (find-divisor n test-divisor)
+  (cond ((> (square test-divisor) n) n)
+        ((divides? test-divisor n) test-divisor)
+        (else (find-divisor n (+ test-divisor 1)))))
+
+(define (divides? a b)
+  (= (remainder b a) 0))
+
+(define (prime? n)
+  (= n (smallest-divisor n)))
+
+;; search-for-primes
+(define (search-for-primes a b)
+  (define (iter a b)
+    (if (< a b)
+        (and (if (prime? a)
+                 (timed-prime-test a))
+             (iter (+ a 1) b))))
+  (iter a b))
+
+;; find the three smallest primes
+(search-for-primes 1000 1020)
+(search-for-primes 10000 10038)
+(search-for-primes 100000 100044)
+(search-for-primes 1000000 1000038)
+(search-for-primes 10000000 10000104)
+(newline)
+
+;; note the time
+;1009 *** 3728
+;1013 *** 3543
+;1019 *** 3252
+;10007 *** 5663
+;10009 *** 4214
+;10037 *** 4386
+;100003 *** 5765
+;100019 *** 5048
+;100043 *** 7159
+;1000003 *** 5907
+;1000033 *** 7467
+;1000037 *** 7219
+;10000019 *** 7371
+;10000079 *** 9883
+;10000103 *** 7781
+
+(test-end :exit-on-failure #t)
